bcb9b852d3eafe020fcf4b3f27db9c879efb11d5
lanim234 <niyi_meg@yahoo.com>
2023-11-05 06:30:09 -0600

    update

--- lana.tf/subnet.tf
+++ lana.tf/subnet.tf
@@ -12,7 +12,7 @@ resource "aws_subnet" "robo-1" {
 resource "aws_subnet" "robo-2" {
   vpc_id     = aws_vpc.main-vpc.id
   cidr_block = "10.0.2.0/24"
-  availability_zone = "us-east-2a"
+  availability_zone = "us-east-1b"
   map_public_ip_on_launch = true
 
   tags = {
