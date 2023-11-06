2bc1012f251622edc9a8a2d6887a094b4ca43179
lanim234 <niyi_meg@yahoo.com>
2023-11-05 18:03:44 -0600

    update

--- lana.tf/iam.tf
+++ lana.tf/iam.tf
@@ -56,16 +56,16 @@ resource "aws_iam_role_policy_attachment" "AmazonEKSVPCResourceController" {
 
 resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.eks-role.name
+  role       = aws_iam_role.node-role.name
 }
 
 resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.eks-role.name
+  role       = aws_iam_role.node-role.name
 }
 
 resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.eks-role.name
+  role       = aws_iam_role.node-role.name
 }
 
