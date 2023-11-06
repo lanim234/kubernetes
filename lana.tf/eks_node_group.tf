471eff877e98cacc38a41bffb94fa13c01714a6d
lanim234 <niyi_meg@yahoo.com>
2023-11-05 18:09:52 -0600

    update

--- lana.tf/eks_node_group.tf
+++ lana.tf/eks_node_group.tf
@@ -1,7 +1,7 @@
 resource "aws_eks_node_group" "node-grp" {
   cluster_name    = aws_eks_cluster.eks.name
   node_group_name = "eks-nodegroup"
-  node_role_arn   = aws_iam_role.eks-role.arn
+  node_role_arn   = aws_iam_role.node-role.arn
   subnet_ids      = [aws_subnet.robo-1.id, aws_subnet.robo-2.id]
   capacity_type = "ON_DEMAND"
   disk_size = "20"
